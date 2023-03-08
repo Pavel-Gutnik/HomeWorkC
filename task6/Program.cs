47b6244083870623ebab6893b1ed5737f623c18a
Pavel <pavel-gutnik@mail.ru>
2023-03-08 23:25:18 +0300

    done task6

---
+++ task6/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Write("введите число: ");
+int x = int.Parse(Console.ReadLine()!);
+if (x % 2 == 0)
+Console.WriteLine($"{x} чётное");
+else
+Console.WriteLine($"{x} нечётное");
+
