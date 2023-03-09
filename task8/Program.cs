daf9cb4aa6687d315c3568c4029565bcb2705502
Pavel <pavel-gutnik@mail.ru>
2023-03-09 19:21:29 +0300

    done task8

---
+++ task8/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i < n)
+{
+  if (i % 2 == 0)
+    Console.Write(i);
+  i++;
+}
