1c4461492501c7a82fb80939e6503a108cfb351a
Pavel <pavel-gutnik@mail.ru>
2023-03-10 20:09:51 +0300

    Семинар 2 задача 10

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Семинар 2 задача 10
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 100 || n > 1000)
+{
+    Console.Write("Вы ошиблись!\nВведите трёхзначное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine((n/10)%10);
+
