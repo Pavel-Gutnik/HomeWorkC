e726c7d0539ef18efa2ca0af76947856d9549cce
Pavel <pavel-gutnik@mail.ru>
2023-03-10 14:53:36 +0300

    Семинар 2 задача 15

---
+++ task15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Семинар 2 задача 15
+Console.Clear();
+Console.Write("Введите число от (1 до 7): ");
+int x = Convert.ToInt32(Console.ReadLine());
+while (x < 1 || x > 7)
+{
+  Console.Write("Вы ошиблись!\nВведите цифру (от 1 до 7): ");
+  x = Convert.ToInt32(Console.ReadLine());
+}
+if (x == 1 || x == 2 || x == 3 || x == 4 || x == 5)
+  Console.WriteLine("этот день не является выходным");
+if (x == 6 || x == 7)
+  Console.WriteLine("этот день является выходным");
