867935980d4bbceb3fcba7d4282a30c5ce3e22c9
Pavel <pavel-gutnik@mail.ru>
2023-03-11 21:40:58 +0300

    семинар 2 задача 13 вариант2 посложнее

---
+++ task13 option 2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Семинар 2 задача 13 второй вариант поинтереснее
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 100)
+{
+  Console.Write("Третьей цифры нет.\nвведите новое число: ");
+  n = Convert.ToInt32(Console.ReadLine());
+}
+{
+  while (n > 999)
+  {
+    n /= 10;
+  }
+  Console.WriteLine(n % 10);
+}
