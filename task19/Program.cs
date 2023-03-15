652e30f7ea1859e80439db56a12c53a1164d85db
Pavel <pavel-gutnik@mail.ru>
2023-03-15 21:17:12 +0300

    Семинар 3 Задача 19

---
+++ task19/Program.cs
@@ -0,0 +1,14 @@
+﻿// Семинаp 3 Задача 19
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string a = Console.ReadLine()!;
+int a0 = int.Parse(a[0].ToString());
+while(a.Length != 5)
+{
+  Console.Write("Ошибка!\nВведите пятизначное число: ");
+  a = Console.ReadLine()!;
+}
+if (a[0] == a[4] && a[1] == a[3])
+  Console.WriteLine("ДА - число является палиандром");
+else
+  Console.WriteLine("НЕТ - число не является палиандром");
