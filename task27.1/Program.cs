f84fab1ee4bd6cc26ada26f58e6c45ff5b2c50ad
Pavel <pavel-gutnik@mail.ru>
2023-03-18 16:58:34 +0300

    Семинар 4 задача 27.1

---
+++ task27.1/Program.cs
@@ -0,0 +1,8 @@
+﻿// программa, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.Write("Введите число: ");
+string s = Console.ReadLine();
+int sum = 0;
+for (int i = 0; i < s.Length; i++)
+    sum = sum + int.Parse(s[i].ToString());
+Console.WriteLine(sum);
