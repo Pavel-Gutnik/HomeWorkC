708d55631b899508eadab108e1772da66372c534
Pavel <pavel-gutnik@mail.ru>
2023-03-30 16:30:43 +0300

    Семинар 7. Задача 52

---
+++ task52/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+void InputMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+      matrix[i, j] = new Random().Next(0, 10);
+  }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+      Console.Write($"{matrix[i, j]} \t");
+    Console.WriteLine();
+  }
+  Console.WriteLine($"Среднее арифметическое столбцов: ");
+}
+void Average(int[,] matrix)
+{
+  for (int j = 0; j < matrix.GetLength(1); j++)
+  {
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+      sum += matrix[i, j];
+    }
+    Console.Write($"{Math.Round((sum / matrix.GetLength(0)), 2)} \t");
+  }
+}
+Console.Clear();
+Console.Write("Введите размерность массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Average(matrix);
