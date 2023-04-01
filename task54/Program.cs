29e48552b60bbfc24f327340b82decf315a631b3
Pavel <pavel-gutnik@mail.ru>
2023-04-01 21:17:26 +0300

    Семинар 8 Задача 54

---
+++ task54/Program.cs
@@ -0,0 +1,46 @@
+﻿// Программа, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+void InputMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+      matrix[i, j] = new Random().Next(0, 10);
+  }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+      Console.Write($"{matrix[i, j]} \t");
+    Console.WriteLine();
+  }
+}
+void SortingRow(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+      {
+        if (matrix[i, k] < matrix[i, k + 1])
+        {
+          int temp = matrix[i, k];
+          matrix[i, k] = matrix[i, k + 1];
+          matrix[i, k + 1] = temp;
+        }
+      }
+    }
+  }
+}
+Console.Clear();
+Console.Write("Введите размерность массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine("Конечный массив: ");
+SortingRow(matrix);
+PrintMatrix(matrix);
